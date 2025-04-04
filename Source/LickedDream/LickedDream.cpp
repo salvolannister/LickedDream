e18be12dbdbc39964dac2282261b93255370d0bc
salvolannister <darksawyer@hotmail.it>
2025-04-04 23:41:06 +0200

    initial file project

---
+++ Source/LickedDream/LickedDream.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LickedDream.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LickedDream, "LickedDream" );
+ 
